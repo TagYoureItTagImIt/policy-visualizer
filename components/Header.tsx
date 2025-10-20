e3d18b5abac415efe88afd5552c947292576bc30
TagYoureItTagImIt <165095077+TagYoureItTagImIt@users.noreply.github.com>
2025-10-20 14:36:57 -0400

    Update Header.tsx for new site name and description

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="text-center p-4 sm:p-6">
-      <h1 className="text-3xl sm:text-4xl font-bold text-gray-100 tracking-tight">Image Color Uniformity Analyzer</h1>
-      <p className="text-md text-gray-400 mt-2">Upload an image to find its most dominant color.</p>
+      <h1 className="text-3xl sm:text-4xl font-bold text-gray-100 tracking-tight">Witbe Policies Visualization Tool</h1>
+      <p className="text-md text-gray-400 mt-2">Upload an image or video visualize policy thresholds for troubleshooting.</p>
     </header>
   );
 };
